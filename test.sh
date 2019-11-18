1d4ddf019576edf5dcf7171157dd3383d8a030af
Dnyaneshwar <dnyaneshwarshinde003@gmail.com>
2019-11-18 18:10:31 +0530

    another

--- test.sh
+++ test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [$(sudo docker container ls -q --filter name=mydb_c)!='']; then
+if [$(docker container ls -q --filter name=mydb_c)!='']; then
 
-    sudo docker container stop mydb_c
-    sudo docker container rm mydb_c
+    docker container stop mydb_c
+    docker container rm mydb_c
 fi
 
-if [$(sudo docker image ls -q --filter reference=mydb)!='']; then
+if [$(docker image ls -q --filter reference=mydb)!='']; then
 
-    sudo docker image rm mydb
+    docker image rm mydb
 fi
 
-sudo docker build . -t mydb
+docker build . -t mydb
 
-sudo docker container run -d -p 3307:3306 --name mydb_c mydb
+docker container run -d -p 3307:3306 --name mydb_c mydb
 
 #mysql -u root -h localhost --port=3307 --protocol=tcp -p
